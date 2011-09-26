bc459a34108f33df121d205e358a1a25dc5ac405
Itiu <ValeriyBushenev@gmail.com>
2011-09-26 18:40:15 +0400

    ++

--- yawl-editor.sh
+++ yawl-editor.sh
@@ -1,2 +1,2 @@
 rm target/lib/jdom-1.0.jar
-java -XX:-UseGCOverheadLimit -Xms256m -Xmx512m -cp ./target/yawl-editor-0.0.1-SNAPSHOT.jar -Djava.library.path=/usr/local/lib org.yawlfoundation.yawl.editor.YAWLEditor
+java -XX:-UseGCOverheadLimit -Xms256m -Xmx512m -cp ./target/yawl-editor-0.0.2-SNAPSHOT.jar -Djava.library.path=/usr/local/lib org.yawlfoundation.yawl.editor.YAWLEditor
